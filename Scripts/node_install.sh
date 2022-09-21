31a307c4cbb03501f3779f180dec67b3b46da51d
AtifSaleem37 <108580110+AtifSaleem37@users.noreply.github.com>
2022-09-21 03:16:56 -0700

    Node Installation using nvm

---
+++ Scripts/node_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 16.13.2
+nvm use 16.13.2
